b81d27ea4f027d713fe80d399ce30e9167bdc173
Alex_ <58192513+MrKelpy@users.noreply.github.com>
2020-08-21 11:39:02 +0100

    1.6 Update

--- Chat.py
+++ Chat.py
@@ -24,30 +24,55 @@ class Chat():
             limit = 50**50
         chat = self.get_chathistory()
         history = []
-        TrueHistory = []
-        for message in chat:
+        parsed_history = []
+        entries = []
+        parsed_entries = []
+        msg = ''
+        for i in chat:
+            entries.append(i.lstrip())
+        for k in entries:
+            if k.startswith('['):
+                if msg != '':
+                    parsed_entries.append(msg)
+                    msg = k
+                else:
+                    msg += k
+            else:
+                msg += k
+        if msg != '':
+            parsed_entries.append(msg)
+        for message in parsed_entries:
             if ']: [CHAT]' in message:
                 msg = message.split(']: [CHAT]')[1]
-                index = chat.index(message)
+                index = parsed_entries.index(message)
                 history.append(f'{msg} |ID|{index}'.strip())
         for i in range(limit):
             history.reverse()
             idbase = history[0].split('|ID|')
             id = idbase[1].strip()
             base = idbase[0]
-            codes = ['§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9',
+            codes = ['Â','§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9',
                      '§a', '§b', '§c', '§d', '§e', '§f',
                      '§k', '§l', '§m', '§n', '§o', '§r']
             try:
                 ' '.join(base).strip()
-                base = base.split(CHAR_BEFORE_NAME)[1]
-                base = base.split(CHAR_AFTER_NAME)
-                user = base[0]
-                base.pop(0)
+                for i in codes:
+                    try:
+                        base = base.split(i)
+                        base = ''.join(base)
+                    except:
+                        continue
+                if base.startswith(CHAR_BEFORE_NAME):
+                    base = base.split(CHAR_BEFORE_NAME)[1]
+                    base = base.split(CHAR_AFTER_NAME)
+                    user = base[0]
+                    base.pop(0)
+                else:
+                    user = None
                 msg = ''.join(base).strip()
                 history.pop(0)
                 affirmation = Affirmation(user.strip(), msg.strip(), id.strip())
-                TrueHistory.append(affirmation)
+                parsed_history.append(affirmation)
                 history.reverse()
             except:
                 try:
@@ -59,22 +84,22 @@ class Chat():
                         except:
                             continue
                     affirmation = Affirmation(None, msg.strip(), id.strip())
-                    TrueHistory.append(affirmation)
+                    parsed_history.append(affirmation)
                     history.pop(0)
                     continue
                 except:
                     continue
-        return TrueHistory
+        return parsed_history
 
 
     def getchat(self, limit: int = 20):
-        if limit is 0:
+        if limit == 0:
             return None
         chat = self.parse_chat_history(limit)
         chat.reverse()
         if chat == []:
             return None
-        if limit is 1:
+        if limit == 1:
             return chat[0]
         else:
             return chat
